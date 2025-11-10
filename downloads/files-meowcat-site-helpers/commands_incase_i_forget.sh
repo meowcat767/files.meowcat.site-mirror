774285f7fbd7aafeaf06542ab157a583475a6267
meowcat767 <benhouse124@gmail.com>
2025-11-10 10:52:30 +0000

    update index

--- downloads/files-meowcat-site-helpers/commands_incase_i_forget.sh
+++ downloads/files-meowcat-site-helpers/commands_incase_i_forget.sh
@@ -1,3 +1,4 @@
 git push --mirror github # push to github mirror - added into git I on deck
 git remote -v # check remotes
 git config pull.rebase true # make sure to rebase on pull incase of conflicts
+python generate_index.py # gen python file
