05be0b6344d31bd4256805e5b337ede865cef16d
meowcat767 <benhouse124@gmail.com>
2025-11-10 10:15:58 +0000

    add commands incase I forget

---
+++ downloads/files-meowcat-site-helpers/commands_incase_i_forget.sh
@@ -0,0 +1,3 @@
+git push --mirror github # push to github mirror - added into git I on deck
+git remote -v # check remotes
+git config pull.rebase true # make sure to rebase on pull incase of conflicts
